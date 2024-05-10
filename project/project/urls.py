7a5d7fc75451c190ec72596e969b493920fb86d8
Fra3z49 <112791937+Fra3z49@users.noreply.github.com>
2024-05-10 11:27:52 -0500

    a

--- project/project/urls.py
+++ project/project/urls.py
@@ -1,28 +1,19 @@
-"""
-URL configuration for project project.
+from django.urls import path
+from django.contrib.auth.decorators import login_required
+from . import views
 
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
-from django.contrib import admin
-from django.urls import path, include, reverse
-from django.contrib.auth import views as oauth_view
+app_name = 'cats'
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path("polls/", include("polls.urls")),
-    path("", include("home.urls")),
-    path("hello/", include("hello.urls")),
-    path("autos/", include("autos.urls")),
-    path("cats/", include("cats.urls"))
+    # URLs for Cats (restricted to logged-in users)
+    path('', login_required(views.CatList.as_view()), name='all'),
+    path('create/', login_required(views.CatCreate.as_view()), name='cat_create'),
+    path('<int:pk>/update/', login_required(views.CatUpdate.as_view()), name='cat_update'),
+    path('<int:pk>/delete/', login_required(views.CatDelete.as_view()), name='cat_delete'),
+    
+    # URLs for Breeds (restricted to logged-in users)
+    path('breeds/', login_required(views.BreedList.as_view()), name='breed_list'),
+    path('breeds/create/', login_required(views.BreedCreate.as_view()), name='breed_create'),
+    path('breeds/<int:pk>/update/', login_required(views.BreedUpdate.as_view()), name='breed_update'),
+    path('breeds/<int:pk>/delete/', login_required(views.BreedDelete.as_view()), name='breed_delete'),
 ]
