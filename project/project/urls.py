2aa600a95587a322585e0d4d06db68db536f20a1
Fra3z49 <112791937+Fra3z49@users.noreply.github.com>
2024-05-10 11:32:40 -0500

    a

--- project/project/urls.py
+++ project/project/urls.py
@@ -1,19 +1,10 @@
-from django.urls import path
-from django.contrib.auth.decorators import login_required
-from . import views
+from django.urls import path, include
 
 app_name = 'cats'
 
 urlpatterns = [
-    # URLs for Cats (restricted to logged-in users)
-    path('', login_required(views.CatList.as_view()), name='all'),
-    path('create/', login_required(views.CatCreate.as_view()), name='cat_create'),
-    path('<int:pk>/update/', login_required(views.CatUpdate.as_view()), name='cat_update'),
-    path('<int:pk>/delete/', login_required(views.CatDelete.as_view()), name='cat_delete'),
-    
-    # URLs for Breeds (restricted to logged-in users)
-    path('breeds/', login_required(views.BreedList.as_view()), name='breed_list'),
-    path('breeds/create/', login_required(views.BreedCreate.as_view()), name='breed_create'),
-    path('breeds/<int:pk>/update/', login_required(views.BreedUpdate.as_view()), name='breed_update'),
-    path('breeds/<int:pk>/delete/', login_required(views.BreedDelete.as_view()), name='breed_delete'),
+    path("", include("home.urls")),
+    path("hello/", include("hello.urls")),
+    path("autos/", include("autos.urls")),
+
 ]
