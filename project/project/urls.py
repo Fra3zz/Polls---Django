07e0c0fdea9ceaf455adb3d2f067ce4b1f9d0503
Fra3z49 <112791937+Fra3z49@users.noreply.github.com>
2024-05-10 12:48:47 -0500

    f

--- project/project/urls.py
+++ project/project/urls.py
@@ -1,13 +1,13 @@
 from django.urls import path, include
+from django.contrib import admin  # Import admin module
 
 app_name = 'cats'
 
 urlpatterns = [
+    path("admin/", admin.site.urls),  # Include admin URLs
     path("", include("home.urls")),
     path("hello/", include("hello.urls")),
     path("autos/", include("autos.urls")),
     path("cats/", include("cats.urls")),
     path("polls/", include("polls.urls"))
-
-
 ]
