d92e648a1b77fbe29ac54ac335aae61cbef61e4b
Fra3z49 <112791937+Fra3z49@users.noreply.github.com>
2024-05-10 10:42:54 -0500

    a

--- project/hello/urls.py
+++ project/hello/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("cookie/", views.cookie, name="cookie"),  # Include URL for function-based view
+    path("", views.cookie, name="cookie"),  # Include URL for function-based view
     path("cookieclass/", views.cookieclass.as_view(), name="cookieclass"),  # Include URL for class-based view
 ]
